a88e312cf70461cc723d007dd23955caf703209c
Beemhuse <brightawah94@gmail.com>
2023-11-03 18:51:41 +0100

    wip:corporate-connect

--- src/pages/CorporateConnect.jsx
+++ src/pages/CorporateConnect.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import Layout from '../components/Layout';
 import Header from './corporate-connect/Header';
+import Why from './corporate-connect/Why';
+import How from './corporate-connect/How';
 
 export default function CorporateConnect() {
   return (
     <Layout>
       <Header />
+      <Why />
+      <How />
     </Layout>
   );
 }
---
+++ src/pages/corporate-connect/Why.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+export default function Why() {
+  const data = [
+    {
+      title: 'STAY UP-TO-DATE',
+      content:
+        'Stay informed with the most recent news, updates, and more from the companies you follow! ',
+      img: 'data.png',
+    },
+    {
+      title: 'NEVER MISS AN UPDATE',
+      content:
+        'Receive instant notifications whenever verified company profiles share new content.',
+      img: 'update.png',
+    },
+    {
+      title: 'MAKE YOUR OPINION COUNT',
+      content:
+        'Take advantage of the Q&A feature to directly ask questions to the participating companies.',
+      img: 'opinion.png',
+    },
+    {
+      title: 'BUILD YOUR COMMUNITY',
+      content:
+        'Engage with fellow investors by voting on the questions you want to see answered.',
+      img: 'build.png',
+    },
+  ];
+  return (
+    <section className='bg-white py-8'>
+      <h2 className='font-bold text-center my-5 text-4xl'>WHY WEBULL CORPORATE CONNECT?</h2>
+
+      <div className='grid grid-cols-2  w-2/3 mx-auto mt-8 gap-8'>
+        {data.map((item, index) => (
+          <div key={index} className='flex gap-4 lg:w-2/3 w-full'>
+            <img src={item.img} className='w-20 h-16' alt="" />
+            <div>
+              <h3 className='font-bold'>{item.title}</h3>
+              <p className='text-gray-500'>{item.content}</p>
+            </div>
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+}
