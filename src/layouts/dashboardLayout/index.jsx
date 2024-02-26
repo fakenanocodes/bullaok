eec7f871c107ee43b144aba8bca6366675bd19fc
asahelcode <hoyx0101@gmail.com>
2024-02-26 09:54:00 +0100

    dashboard home complete

--- src/layouts/dashboardLayout/index.jsx
+++ src/layouts/dashboardLayout/index.jsx
@@ -55,7 +55,7 @@ const DashboardLayout = () => {
               />
             </div>
           </div>
-          <div className="  mx-7 h-[75vh] border bg-black bg-opacity-[62%] rounded-r-xl p-10">
+          <div className="  mx-7 h-[75vh] border bg-black bg-opacity-[62%] rounded-r-xl p-10 overflow-y-scroll">
             <Outlet />
           </div>
         </div>
--- src/pages/dashboard/DashboardHome.jsx
+++ src/pages/dashboard/DashboardHome.jsx
@@ -1,10 +1,33 @@
 import ArrowDropUpIcon from '@mui/icons-material/ArrowDropUp';
 import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
 import Chart from '../../assets/dashboard/chart.svg';
+import assetImage from '../../assets/dashboard/investment/assets.png';
+import cryptoImage from '../../assets/dashboard/investment/crypto.png';
+import forexImage from '../../assets/dashboard/investment/forex.png';
+import realEstateImage from '../../assets/dashboard/investment/realestate.png';
+
+const investments = [
+  {
+    label: 'Assets',
+    imgUrl: assetImage,
+  },
+  {
+    label: 'Real Estate',
+    imgUrl: realEstateImage,
+  },
+  {
+    label: 'Crypto',
+    imgUrl: cryptoImage,
+  },
+  {
+    label: 'Forex',
+    imgUrl: forexImage,
+  },
+];
 
 const DashboardHome = () => {
   return (
-    <div className="text-white">
+    <div className="space-y-3">
       <div className="w-full flex gap-8">
         <div className="border rounded-sm bg-[#0C0000] border-white border-opacity-40 w-3/5 h-72 p-5 pl-16 flex flex-col space-y-8">
           <span className="text-[#868383] text-lg">Current Balance</span>
@@ -52,6 +75,78 @@ const DashboardHome = () => {
                 <span>Cannabis</span>
               </span>
               <span className="text-[#868383]">$7,886</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="space-y-3">
+        <div className="w-full flex px-3 justify-between">
+          <span className="font-semibold">All Investment</span>
+          <span className="text-[#F5C249]">See all</span>
+        </div>
+        <div className="flex justify-between">
+          {investments.map((investment, index) => (
+            // eslint-disable-next-line react/jsx-key
+            <div className="flex justify-center items-center flex-col border p-4 gap-3 bg-[#0C0000] border-[#0C0000]">
+              <span className="text-lg font-medium">{investment?.label}</span>
+              <img src={investment?.imgUrl} alt="" />
+              <button className="w-full border py-2 font-semibold hover:bg-[#F8DF9F] hover:text-black">
+                Invest
+              </button>
+            </div>
+          ))}
+        </div>
+        <div className="flex flex-col space-y-2">
+          <div className="flex justify-end">
+            <span className="text-[#F5C249]">View all History</span>
+          </div>
+          <div className="w-full space-y-1">
+            {/* <table>
+              <thead>
+                <tr className="bg-[#924E8F] flex justify-between">
+                  <th>Name</th>
+                  <th>Detail</th>
+                  <th></th>
+                  <th>Date</th>
+                </tr>
+              </thead>
+            </table> */}
+            <div className="flex justify-between px-10 py-2 bg-[#924E8F]">
+              <span>Name</span>
+              <span className="pr-12">Detail</span>
+              <span className="pr-12">Date</span>
+            </div>
+            <div className="flex flex-col space-y-1">
+              <div className="flex justify-between px-10 py-4 bg-[#BB9FB3] bg-opacity-[38%]">
+                <span className="flex items-center gap-2">
+                  <span className="bg-white w-6 h-6 flex justify-center items-center rounded-full">
+                    <FiberManualRecordIcon className="text-[#6B5F6B] rounded-full" />
+                  </span>
+                  Real estate
+                </span>
+                <span>You invested $578,8933 on real estate plan</span>
+                <span>January 25th... 15:03PM</span>
+              </div>
+              <div className="flex justify-between px-10 py-4 bg-[#BB9FB3] bg-opacity-[38%]">
+                <span className="flex items-center gap-2">
+                  <span className="bg-white w-6 h-6 flex justify-center items-center rounded-full">
+                    <FiberManualRecordIcon className="text-[#732220] rounded-full" />
+                  </span>
+                  Real estate
+                </span>
+                <span>You invested $578,8933 on real estate plan</span>
+                <span>January 25th... 15:03PM</span>
+              </div>
+              <div className="flex justify-between px-10 py-4 bg-[#BB9FB3] bg-opacity-[38%]">
+                <span className="flex items-center gap-2">
+                  <span className="bg-white w-6 h-6 flex justify-center items-center rounded-full">
+                    <FiberManualRecordIcon className="text-[#FFB803] rounded-full" />
+                  </span>
+                  Real estate
+                </span>
+                <span>You invested $578,8933 on real estate plan</span>
+                <span>January 25th... 15:03PM</span>
+              </div>
             </div>
           </div>
         </div>
