ce6f1bc53b016e773086bba266be690e7f4f0e82
techpilot <ngwustephen99@gmail.com>
2024-02-28 00:47:03 -0800

    ci: ui updates

--- src/components/card_application_setup/DeliveryOption.jsx
+++ src/components/card_application_setup/DeliveryOption.jsx
@@ -1,8 +1,24 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { Controller, useForm } from 'react-hook-form';
+import * as yup from 'yup';
 import CancelIcon from '../utils/reusables/CancelIcon';
 import CardInputComponent from '../utils/reusables/CardInputComponent';
 import ForwardArrowIcon from '../utils/reusables/ForwardArrowIcon';
 
+const schema = yup.object().shape({
+  delivery_method: yup.string(),
+  zip_code: yup.string(),
+  city: yup.string(),
+  area: yup.string(),
+});
+
 const DeliveryOption = () => {
+  const { control, getValues } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  console.log(getValues());
+
   return (
     <div className="py-3 px-14">
       <p className="text-sm md:text-base lg:text-xl text-black font-[poppins]">
@@ -10,6 +26,41 @@ const DeliveryOption = () => {
       </p>
 
       <form className="mt-[1.5rem] flex flex-col gap-3">
+        <div className="flex items-center gap-8">
+          <Controller
+            name="delivery_method"
+            control={control}
+            render={({ field }) => (
+              <div className="flex items-center gap-3">
+                <input
+                  className="cursor-pointer accent-[#8E0789] text-[#8E0789] outline-none"
+                  type="radio"
+                  {...field}
+                  id="courierDelivery"
+                  value="courierDelivery"
+                />
+
+                <p className="text-black font-[poppins]">Courier delivery</p>
+              </div>
+            )}
+          />
+          <Controller
+            name="delivery_method"
+            control={control}
+            render={({ field }) => (
+              <div className="flex items-center gap-3">
+                <input
+                  className="cursor-pointer accent-[#8E0789] text-[#8E0789]"
+                  type="radio"
+                  {...field}
+                  id="officeDelivery"
+                  value="officeDelivery"
+                />
+                <p className="text-black font-[poppins]">Deliver at office</p>
+              </div>
+            )}
+          />
+        </div>
         <div className="flex justify-between items-center gap-5 w-full">
           <div className="w-full">
             <CardInputComponent label={'Zip code'} required={true} />
@@ -24,7 +75,7 @@ const DeliveryOption = () => {
           </div>
         </div>
 
-        <div className="flex items-center gap-5 mt-5">
+        <div className="flex items-center gap-5 mt-10">
           <button className="h-[2.4rem] flex items-center gap-1 px-3 py-2 text-sm text-[#5F5656] font-semibold border border-[#41073F] rounded-sm">
             Cancel
             <CancelIcon />
