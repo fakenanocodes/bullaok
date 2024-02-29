0a52605fa24cea20ede75e2c3d4f21b601a831e7
Aniede Chibundu <ndubest56@gmail.com>
2024-02-29 23:24:55 +0100

    reactor code

--- src/pages/dashboard/transactions/saveItems.js
+++ src/pages/dashboard/transactions/saveItems.js
@@ -1,26 +1,26 @@
-// import { useDispatch } from 'react-redux';
-// import { useNavigate } from 'react-router-dom';
-// import {
-//   setDeposite,
-//   setTransfer,
-//   setWithDrawal,
-// } from '../../store/reducers/transact_reducer';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import {
+  setDeposite,
+  setTransfer,
+  setWithDrawal,
+} from '../../store/reducers/transact_reducer';
 
-// const navigate = useNavigate();
-// const dispatch = useDispatch();
+const navigate = useNavigate();
+const dispatch = useDispatch();
 
-// const handleWitDrawal = () => {
-//   dispatch(setWithDrawal(true));
-//   dispatch(setDeposite(false));
-//   dispatch(setTransfer(false));
-// };
-// const handleWitDeposit = () => {
-//   dispatch(setWithDrawal(false));
-//   dispatch(setDeposite(true));
-//   dispatch(setTransfer(false));
-// };
-// const handleWitTransfer = () => {
-//   dispatch(setWithDrawal(false));
-//   dispatch(setDeposite(false));
-//   dispatch(setTransfer(true));
-// };
+const handleWitDrawal = () => {
+  dispatch(setWithDrawal(true));
+  dispatch(setDeposite(false));
+  dispatch(setTransfer(false));
+};
+const handleWitDeposit = () => {
+  dispatch(setWithDrawal(false));
+  dispatch(setDeposite(true));
+  dispatch(setTransfer(false));
+};
+const handleWitTransfer = () => {
+  dispatch(setWithDrawal(false));
+  dispatch(setDeposite(false));
+  dispatch(setTransfer(true));
+};
