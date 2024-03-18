169eea0edffd3c8f9038987531b8e4b5b53fd872
Beemhuse <brightawah94@gmail.com>
2024-03-18 08:40:40 -0700

    fix

--- src/api/config.js
+++ src/api/config.js
@@ -41,10 +41,8 @@ export default function init() {
   // Set base URL for Axios
   axios.defaults.baseURL = API_URL;
 
-  // Set withCredentials to false (unless you need to include credentials in cross-origin requests)
   axios.defaults.withCredentials = false;
 
-  // Check if the token is available and not expired
   if (token) {
     // Set Authorization header with the token
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
