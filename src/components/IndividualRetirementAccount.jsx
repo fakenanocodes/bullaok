255fc7f56e652843f1dd2d13a48973159c2e8d69
Izutech <inspire.ig57@gmail.com>
2024-05-16 14:05:19 +0100

    added semi doughnut chart to Ira page

--- src/components/IndividualRetirementAccount.jsx
+++ src/components/IndividualRetirementAccount.jsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React,{ useState } from 'react';
+import {Doughnut} from 'react-chartjs-2'
+import {Chart as ChartJS, plugins} from 'chart.js/auto'
 
 const IndividualRetirementAccount = () => {
   const retirementData = [
@@ -15,6 +17,48 @@ const IndividualRetirementAccount = () => {
       amount: '$ 0.00',
     },
   ];
+
+  // data for the doughnuts
+  const accountAnalyticsData = [
+    {
+      title: 'Saving balance',
+      amount: 2474.91,
+      percentage: '50%',
+      color: '#FFB803',
+    },
+    {
+      title: 'REtirement balance',
+      amount: 1274.91,
+      percentage: '30%',
+      color: '#F324EC',
+    },
+  ];
+  const [doughnut,setDoughnut] = useState({
+    labels: accountAnalyticsData.map(data => data.title),
+    datasets: [{
+      // label:  accountAnalyticsData.map(data => data.title),
+      data: accountAnalyticsData.map(data => data.amount),
+      backgroundColor: accountAnalyticsData.map(data => data.color),
+      borderWidth:0,
+      // boxShadow: '2 2 yellow',
+    }]
+  })
+  const [option,setOption] = useState({
+    responsive:true,
+    maintainAspectRatio: false,
+    rotation: -90,
+    circumference: 180,
+    cutoutPercentage: 70,
+    legend: {
+      display: false,
+      position: 'bottom'
+    }
+  })
+  ChartJS.defaults.height = '60px';
+  ChartJS.defaults.width = '60px';
+  window.addEventListener('resize',()=>{
+    doughnut.resize()
+  })
   return (
     <section className=" h-full px-6 font-poppins">
       <div className="flex gap-5  my-6 flex-col sm:flex-row  ">
@@ -58,12 +102,19 @@ const IndividualRetirementAccount = () => {
             Month
           </button>
         </article>
-        <article className="flex flex-col sm:flex-row justify-between items-center">
-          <h1>CHARTS</h1>
-          <button className="  w-full sm:w-[40%] h-[47px] rounded-md border-[0.75px] border-[#8C89B480] text-[#AEABD8]  px-2 text-sm ">
-            View all activity
-          </button>
-        </article>
+        <div className="flex flex-col sm:flex-row justify-between items-center h-fit py-3 relative overflow-hidden">
+            <div className='w-full flex items-center justify-center'>
+              <Doughnut data={doughnut} options={option} className='h-full'/>
+              <div>
+                <div>
+                  <div className='w-[20px] h-[20px] bg-[#e747f6] rounded-[50%]'></div>
+                </div> 
+              </div>
+            </div>
+            <button className="  w-full sm:w-[40%] h-[47px] rounded-md border-[0.75px] border-[#8C89B480] text-[#AEABD8]  px-2 text-sm hidden">
+              View all activity
+            </button>
+        </div>
       </div>
       <div className="w-full bg-[#000000] p-4  rounded-[12px]">
         <article className="flex justify-between text-white">
