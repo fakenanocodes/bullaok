afa2d871ad49493a6a674a6226b58756c49c9c25
Izutech <inspire.ig57@gmail.com>
2024-10-02 17:05:40 +0100

    fix: corrected returning message wen not verified and routing to verification page

---
+++ src/pages/Verify.jsx
@@ -0,0 +1,12 @@
+import VerificationComponent from '../components/auth/signup/VerificationComponent';
+import MainLayout from '../components/MainLayout';
+
+const Verify = () => {
+  return (
+    <MainLayout>
+      <VerificationComponent />
+    </MainLayout>
+  );
+};
+
+export default Verify;
