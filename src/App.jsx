4c18d829a9432772ac820baca7f9080c93234570
Izutech <inspire.ig57@gmail.com>
2024-11-12 16:56:11 +0100

    fix: change translator

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-import { RouterProvider } from 'react-router-dom';
+import { RouterProvider, useLocation } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import router from './router/index';
 import { useEffect } from 'react';
 function App() {
-  useEffect(() => {
-    if (window.Weglot) {
-      window.Weglot.initialize({
-        api_key: 'wg_dabedcebf2c2bdad2ca4be7d61f92b590'
-      });
-    }
-  }, []);
+
+
+  // const location = useLocation();
+  
+  // useEffect(() => {
+  //   if (window.Weglot) {
+
+  //     window.Weglot.initialize({
+  //       api_key: 'wg_dabedcebf2c2bdad2ca4be7d61f92b590',
+  //     });
+  //   }
+  // }, []);
   return (
     <>
       <ToastContainer />
