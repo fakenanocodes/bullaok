7d59690732fadc6a2540ddf0b5e1e420c0fda561
d-inspire <inspire.ig57@gmail.com>
2025-10-02 09:08:03 +0100

    fix: fixed the issue of eth wallet address having the same value as the btc

--- src/pages/dashboard/transactions/Deposit.jsx
+++ src/pages/dashboard/transactions/Deposit.jsx
@@ -43,7 +43,7 @@ const Deposit = () => {
 // walletMock
   const walletMock = {
     BTC: walletAddress?.bitcoin_address,
-    ETH: walletAddress?.etherum_address, // Fixed typo here
+    ETH: walletAddress?.ethereum_address, // Fixed typo here
     LTC: walletAddress?.litecoin_address,
     USDT: walletAddress?.usdt_address,
     XRP: walletAddress?.xrp_address,
