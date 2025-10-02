eb299b7602bb9d9e39e0f8fada16539b301c2082
d-inspire <inspire.ig57@gmail.com>
2025-10-02 09:12:14 +0100

    fix: fixed the issue of eth wallet address having the same value as the btc

--- src/pages/dashboard/transactions/Deposit.jsx
+++ src/pages/dashboard/transactions/Deposit.jsx
@@ -26,7 +26,7 @@ const Deposit = () => {
   const { data: user } = useSWR(`/user/`);
   const { data: walletAddress } = useSWR('/walletaddress/');
 
-  console.log("wallet",walletAddress);
+  // console.log("wallet",walletAddress);
   
   const navigate = useNavigate();
 
